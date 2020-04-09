c6e93a2fe3fd2dff856a08ac8be7d2a5c26e6185
MinhKhue_Home <minhkhue1@gmail.com>
2020-04-09 18:00:21 +0700

    FirstUpload

---
+++ CalculatorTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalculatorTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalculatorTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("902f8a2b-e9a7-48c5-aa7f-6f5a862236d7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CalculatorTest/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NUnit.Framework;
+using CalculatorProject;
+
+namespace CalculatorTest
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        private Calculator cal;
+
+        [SetUp]
+        public void Setup()
+        {
+            cal = new Calculator();
+        }
+
+        [Test]
+        public void OnePlusOneEqualTwo()
+        {
+            Assert.AreEqual(2, cal.Add(1, 1));
+        }
+
+        [Test]
+        public void TwoPlusTwoEqualFour()
+        {
+            Assert.AreEqual(4, cal.Add(2, 2));
+        }
+
+        [Test]
+        public void FourPlusOneEqualFive()
+        {
+            Assert.AreEqual(5, cal.Add(4, 1));
+        }
+    }
+}
